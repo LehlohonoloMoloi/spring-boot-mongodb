c9f39c101a92fd00f6a73a36ac354377ce40f7da
Lehlohonolo Moloi <lehlohonolo@bbd.co.za>
2024-03-30 00:16:41 +0200

    Added missing files.

---
+++ spring-boot-mongodb-tut/src/main/java/com/programming/techie/springbootmongodbtut/SpringBootMongodbTutApplication.java
@@ -0,0 +1,13 @@
+package com.programming.techie.springbootmongodbtut;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMongodbTutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMongodbTutApplication.class, args);
+	}
+
+}
